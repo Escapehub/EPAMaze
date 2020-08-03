745c5bb2f0c642dcd1f1fc71ad721f81897468e9
Callum <54590992+Escapehub@users.noreply.github.com>
2020-08-03 12:22:14 +0100

    Added tilemap class

---
+++ EPAMaze/tilemap.h
@@ -0,0 +1,15 @@
+#ifndef TILEMAP_H
+#define TILEMAP_H
+#include <SFML/Graphics.hpp>
+
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+public:
+	bool load(const std::string&, sf::Vector2u, const int*, unsigned int, unsigned int);
+private:
+	virtual void draw(sf::RenderTarget&, sf::RenderStates) const;
+	sf::VertexArray m_vertices;
+	sf::Texture m_tileset;
+};
+
+#endif 
